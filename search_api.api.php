a683488bb85bb5ad427e8ddc259e08861326b349
Thomas Seidl <remus@gmx.net>
2010-06-15 21:12:00 +0000

    Expanded functionality, implemented functions, added some hooks and did some refactorization (but still more planned). Bit of a huge commit, I guess.

--- search_api.api.php
+++ search_api.api.php
@@ -44,5 +44,62 @@ function hook_search_api_backend_classes() {
 }
 
 /**
+ * Registers one or more callbacks that can be called at index time to add
+ * additional data to the indexed items (e.g. comments or attachments to nodes),
+ * alter the data in other forms or remove items from the array.
+ *
+ * For the required signature of callbacks, see example_random_alter().
+ *
+ * @return An associative array keyed by the function names and containing
+ *   arrays with the following keys:
+ *   - name: The name to display for this callback.
+ *   - description: A short description of what the callback does.
+ *   - enabled: (optional) Whether this callback should be enabled by default.
+ *     Defaults to TRUE.
+ *   - weight: (optional) Defines the order in which callbacks are displayed
+ *     (and, therefore, invoked) by default. Defaults to 0.
+ *
+ * @see example_random_alter
+ */
+function hook_search_api_register_alter_callback() {
+  $callbacks['example_random_alter'] = array(
+    'name' => t('Random alteration'),
+    'description' => t('Alters all passed item data completely randomly.'),
+    'enabled' => FALSE,
+    'weight' => 100,
+  );
+  $callbacks['example_add_comments'] = array(
+    'name' => t('Add comments'),
+    'description' => t('For nodes and similar entities, adds comments.'),
+  );
+
+  return $callbacks;
+}
+
+/**
+ * Search API data alteration callback that randomly changes item data.
+ *
+ * @param $phase The phase in which this call occurs. Either 'index' or
+ *   'search'.
+ * @param $index The index on which the items are indexed.
+ * @param $items An array of objects containing the entity data.
+ */
+function example_random_alter($phase, $index, &$items) {
+  if ($phase == 'index' && $index->id % 2) {
+    foreach ($items as $id => $item) {
+      srand($id);
+      if (rand() % 5) {
+        unset($items[$id]);
+        continue;
+      }
+      foreach ($item as $k => $v) {
+        srand(strlen($v) + count($v));
+        $item->$k = rand();
+      }
+    }
+  }
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
