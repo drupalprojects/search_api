8619a668bf4840aceb4bd9d9846062069f302f98
Thomas Seidl <remus@gmx.net>
2014-03-24 13:01:55 +0100

    Removed IdentifiableInterface.

--- lib/Drupal/search_api/Datasource/Item/ItemInterface.php
+++ lib/Drupal/search_api/Datasource/Item/ItemInterface.php
@@ -6,11 +6,10 @@
 
 namespace Drupal\search_api\Datasource\Item;
 
-use Drupal\Core\TypedData\IdentifiableInterface;
 use Drupal\Core\TypedData\ComplexDataInterface;
 use Drupal\Core\TypedData\TranslatableInterface;
 
 /**
  * Interface which describes a datasource item.
  */
-interface ItemInterface extends IdentifiableInterface, ComplexDataInterface, TranslatableInterface { }
+interface ItemInterface extends ComplexDataInterface, TranslatableInterface { }
