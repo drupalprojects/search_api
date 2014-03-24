6086cc825e42842ce3df12df4c75c98d7007426f
Thomas Seidl <remus@gmx.net>
2014-03-24 23:10:55 +0100

    Revert "Removed static create() method for plugins again."
    This reverts commit e97d95e176278caa6c2443f61b7efaa1d2ab7476.

--- lib/Drupal/search_api/Plugin/ConfigurablePluginInterface.php
+++ lib/Drupal/search_api/Plugin/ConfigurablePluginInterface.php
@@ -6,14 +6,15 @@
 
 namespace Drupal\search_api\Plugin;
 
-use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Component\Plugin\ConfigurablePluginInterface as DrupalConfigurablePluginInterface;
+use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
 
 /**
  * Interface which describes a plugin for Search API.
  */
-interface ConfigurablePluginInterface extends PluginInspectionInterface, DrupalConfigurablePluginInterface, PluginFormInterface {
+interface ConfigurablePluginInterface extends PluginInspectionInterface, DrupalConfigurablePluginInterface, PluginFormInterface, ContainerFactoryPluginInterface {
 
   /**
    * Get the label for use on the administration pages.
