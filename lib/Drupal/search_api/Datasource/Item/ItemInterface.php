91b297f07275e97eb26063f71c9fdf0850585793
Thomas Seidl <remus@gmx.net>
2014-03-24 13:01:05 +0100

    Removed another superfluous comment.

--- lib/Drupal/search_api/Datasource/Item/ItemInterface.php
+++ lib/Drupal/search_api/Datasource/Item/ItemInterface.php
@@ -6,9 +6,6 @@
 
 namespace Drupal\search_api\Datasource\Item;
 
-/*
- * Include the required classes and interfaces.
- */
 use Drupal\Core\TypedData\IdentifiableInterface;
 use Drupal\Core\TypedData\ComplexDataInterface;
 use Drupal\Core\TypedData\TranslatableInterface;
