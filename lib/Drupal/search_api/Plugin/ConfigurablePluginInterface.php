b8d83dfddf2912ff6fee46fb7ee38c503e09cefd
Thomas Seidl <remus@gmx.net>
2014-03-24 22:46:15 +0100

    Add a static create() method for plugins.

--- lib/Drupal/search_api/Plugin/ConfigurablePluginInterface.php
+++ lib/Drupal/search_api/Plugin/ConfigurablePluginInterface.php
@@ -9,11 +9,12 @@ namespace Drupal\search_api\Plugin;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Component\Plugin\ConfigurablePluginInterface as DrupalConfigurablePluginInterface;
 use Drupal\Core\Plugin\PluginFormInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 
 /**
  * Interface which describes a plugin for Search API.
  */
-interface ConfigurablePluginInterface extends PluginInspectionInterface, DrupalConfigurablePluginInterface, PluginFormInterface {
+interface ConfigurablePluginInterface extends PluginInspectionInterface, DrupalConfigurablePluginInterface, PluginFormInterface, ContainerInjectionInterface {
 
   /**
    * Get the label for use on the administration pages.
