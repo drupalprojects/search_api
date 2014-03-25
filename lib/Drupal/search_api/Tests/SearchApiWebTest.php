2c81588afeabbef41164eed9f57983f8216c3670
Mattias Michaux <mattias.michaux@gmail.com>
2014-03-25 23:17:33 +0100

    Fix for incorrect spaces in SearchApiWebTest

--- lib/Drupal/search_api/Tests/SearchApiWebTest.php
+++ lib/Drupal/search_api/Tests/SearchApiWebTest.php
@@ -13,183 +13,182 @@ use Drupal\simpletest\WebTestBase;
  */
 class SearchApiWebTest extends WebTestBase {
 
-    public static $modules = array('search_api', 'node');
+  public static $modules = array('search_api', 'node');
 
-    protected $test_user;
-    protected $server_id;
+  protected $testUser;
+  protected $serverId;
+  protected $indexId;
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function getInfo() {
-        return array(
-            'name' => 'Search API web tests',
-            'description' => 'Tests for Search API to see if the interface reacts as it should.',
-            'group' => 'Search API',
-        );
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Search API web tests',
+      'description' => 'Tests for Search API to see if the interface reacts as it should.',
+      'group' => 'Search API',
+    );
+  }
 
-    public function setUp() {
-        parent::setUp();
+  public function setUp() {
+    parent::setUp();
 
-        // Create user with Search API permissions.
-        $this->test_user = $this->drupalCreateUser(array('administer search_api', 'access administration pages'));
-    }
+    // Create user with Search API permissions.
+    $this->test_user = $this->drupalCreateUser(array('administer search_api', 'access administration pages'));
+  }
 
-    public function testFramework() {
+  public function testFramework() {
 
-        $this->drupalLogin($this->test_user);
-        $this->renderMenuLinkTest();
-        $this->createServer();
-        $this->createIndex();
-        $this->addFieldsToIndex();
-        $this->addAdditionalFieldsToIndex();
-        $this->addFiltersToIndex();
-    }
+    $this->drupalLogin($this->testUser);
+    $this->renderMenuLinkTest();
+    $this->createServer();
+    $this->createIndex();
+    $this->addFieldsToIndex();
+    $this->addAdditionalFieldsToIndex();
+    $this->addFiltersToIndex();
+  }
 
-    public function createServer() {
+  public function renderMenuLinkTest() {
+    $this->drupalGet('admin/config');
+    $this->assertText('Search API', 'Search API menu link is displayed.');
+  }
 
-        $settings_path = 'admin/config/search/search_api/add_server';
+  public function createServer() {
 
-        $this->drupalGet($settings_path);
-        $this->assertResponse(200);
-        $edit = array(
-            'name' => '',
-            'status' => 1,
-            'description' => 'A server used for testing.',
-            'servicePluginId' => '',
-        );
+    $settings_path = 'admin/config/search/search_api/add_server';
 
-        $this->drupalPostForm($settings_path, $edit, t('Save'));
-        $this->assertText(t('!name field is required.', array('!name' => t('Server name'))));
-        $this->assertText(t('!name field is required.', array('!name' => t('Service'))));
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200);
+    $edit = array(
+      'name' => '',
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'servicePluginId' => '',
+    );
 
+    $this->drupalPostForm($settings_path, $edit, t('Save'));
+    $this->assertText(t('!name field is required.', array('!name' => t('Server name'))));
+    $this->assertText(t('!name field is required.', array('!name' => t('Service'))));
 
-        $edit = array(
-            'name' => 'Search API test server',
-            'status' => 1,
-            'description' => 'A server used for testing.',
-            'servicePluginId' => 'search_api_test_service',
-        );
-        $this->drupalPostForm($settings_path, $edit, t('Save'));
-        $this->assertText(t('!name field is required.', array('!name' => t('Machine-readable name'))));
+    $edit = array(
+      'name' => 'Search API test server',
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'servicePluginId' => 'search_api_test_service',
+    );
+    $this->drupalPostForm($settings_path, $edit, t('Save'));
+    $this->assertText(t('!name field is required.', array('!name' => t('Machine-readable name'))));
 
-        $this->server_id = 'test_server';
+    $this->server_id = 'test_server';
 
-        $edit = array(
-            'name' => 'Search API test server',
-            'machine_name' => $this->server_id,
-            'status' => 1,
-            'description' => 'A server used for testing.',
-            'servicePluginId' => 'search_api_test_service',
-        );
+    $edit = array(
+      'name' => 'Search API test server',
+      'machine_name' => $this->serverId,
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'servicePluginId' => 'search_api_test_service',
+    );
 
-        // The first post gives a 'Please configure the used service.' warning, so we have to submit the form twice.
-        $this->drupalPostForm(NULL, $edit, t('Save'));
-        $this->drupalPostForm(NULL, $edit, t('Save'));
+    // The first post gives a 'Please configure the used service.' warning,
+    // so we have to submit the form twice.
+    $this->drupalPostForm(NULL, $edit, t('Save'));
+    $this->drupalPostForm(NULL, $edit, t('Save'));
 
-        $this->assertText(t('The server was successfully saved.'));
-        $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/server/' . $this->server_id) !== FALSE;
-        $this->assertTrue($redirect_path, t('Correct redirect.'));
-    }
+    $this->assertText(t('The server was successfully saved.'));
+    $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/server/' . $this->server_id) !== FALSE;
+    $this->assertTrue($redirect_path, t('Correct redirect.'));
+  }
 
-    public function createIndex() {
+  public function createIndex() {
 
-        $settings_path = 'admin/config/search/search_api/add_index';
+    $settings_path = 'admin/config/search/search_api/add_index';
 
-        $this->drupalGet($settings_path);
-        $this->assertResponse(200);
-        $edit = array(
-            'name' => '',
-            'status' => 1,
-            'description' => 'An index used for testing.',
-            'datasourcePluginId' => '',
-        );
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200);
+    $edit = array(
+      'name' => '',
+      'status' => 1,
+      'description' => 'An index used for testing.',
+      'datasourcePluginId' => '',
+    );
 
-        $this->drupalPostForm($settings_path, $edit, t('Save'));
-        $this->assertText(t('!name field is required.', array('!name' => t('Index name'))));
-        $this->assertText(t('!name field is required.', array('!name' => t('Datasource'))));
+    $this->drupalPostForm($settings_path, $edit, t('Save'));
+    $this->assertText(t('!name field is required.', array('!name' => t('Index name'))));
+    $this->assertText(t('!name field is required.', array('!name' => t('Datasource'))));
 
-        $this->index_id = 'test_index';
+    $this->indexId = 'test_index';
 
-        $edit = array(
-            'name' => 'Search API test index',
-            'machine_name' => $this->index_id,
-            'status' => 1,
-            'description' => 'An index used for testing.',
-            'serverMachineName' => $this->server_id,
-            'datasourcePluginId' => 'search_api_content_entity_datasource:node',
-        );
+    $edit = array(
+      'name' => 'Search API test index',
+      'machine_name' => $this->indexId,
+      'status' => 1,
+      'description' => 'An index used for testing.',
+      'serverMachineName' => $this->serverId,
+      'datasourcePluginId' => 'search_api_content_entity_datasource:node',
+    );
 
-        $this->drupalPostForm(NULL, $edit, t('Save'));
-        $this->drupalPostForm(NULL, $edit, t('Save'));
+    $this->drupalPostForm(NULL, $edit, t('Save'));
+    $this->drupalPostForm(NULL, $edit, t('Save'));
 
-        $this->assertText(t('The index was successfully saved.'));
-        $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->index_id) !== FALSE;
-        $this->assertTrue($redirect_path, t('Correct redirect to index page.'));
+    $this->assertText(t('The index was successfully saved.'));
+    $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->indexId) !== FALSE;
+    $this->assertTrue($redirect_path, t('Correct redirect to index page.'));
 
-        $index = entity_load('search_api_index', $this->index_id, TRUE);
+    $index = entity_load('search_api_index', $this->indexId, TRUE);
 
-        $this->assertEqual($index->name, $edit['name'], t('Name correctly inserted.'));
-        $this->assertEqual($index->machine_name, $edit['machine_name'], t('Index machine name correctly inserted.'));
-        $this->assertTrue($index->status, t('Index status correctly inserted.'));
-        $this->assertEqual($index->description, $edit['description'], t('Index machine name correctly inserted.'));
-        $this->assertEqual($index->serverMachineName, $edit['serverMachineName'], t('Index server machine name correctly inserted.'));
-        $this->assertEqual($index->datasourcePluginId, $edit['datasourcePluginId'], t('Index datasource id correctly inserted.'));
-    }
+    $this->assertEqual($index->name, $edit['name'], t('Name correctly inserted.'));
+    $this->assertEqual($index->machine_name, $edit['machine_name'], t('Index machine name correctly inserted.'));
+    $this->assertTrue($index->status, t('Index status correctly inserted.'));
+    $this->assertEqual($index->description, $edit['description'], t('Index machine name correctly inserted.'));
+    $this->assertEqual($index->serverMachineName, $edit['serverMachineName'], t('Index server machine name correctly inserted.'));
+    $this->assertEqual($index->datasourcePluginId, $edit['datasourcePluginId'], t('Index datasource id correctly inserted.'));
+  }
 
-    public function addFieldsToIndex() {
-        $settings_path = 'admin/config/search/search_api/index/' . $this->index_id . '/fields';
+  public function addFieldsToIndex() {
+    $settings_path = 'admin/config/search/search_api/index/' . $this->indexId . '/fields';
 
-        $this->drupalGet($settings_path);
-        $this->assertResponse(200);
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200);
 
-        $edit = array(
-            'fields[entity:node:nid][indexed]' => 1,
-            'fields[entity:node:title][indexed]' => 1,
-            'fields[entity:node:title][type]' => 'text',
-            'fields[entity:node:title][boost]' => '21.0',
-        );
+    $edit = array(
+      'fields[entity:node:nid][indexed]' => 1,
+      'fields[entity:node:title][indexed]' => 1,
+      'fields[entity:node:title][type]' => 'text',
+      'fields[entity:node:title][boost]' => '21.0',
+    );
 
-        $this->drupalPostForm($settings_path, $edit, t('Save changes'));
-        $this->assertText(t('The indexed fields were successfully changed. The index was cleared and will have to be re-indexed with the new settings.'));
-        $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->index_id . '/fields') !== FALSE;
-        $this->assertTrue($redirect_path, t('Correct redirect to fields page.'));
+    $this->drupalPostForm($settings_path, $edit, t('Save changes'));
+    $this->assertText(t('The indexed fields were successfully changed. The index was cleared and will have to be re-indexed with the new settings.'));
+    $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->indexId . '/fields') !== FALSE;
+    $this->assertTrue($redirect_path, t('Correct redirect to fields page.'));
 
-        $index = entity_load('search_api_index', $this->index_id, TRUE);
-        $fields = $index->getFields();
+    $index = entity_load('search_api_index', $this->indexId, TRUE);
+    $fields = $index->getFields();
 
-        $this->assertEqual($fields['entity:node:nid']['indexed'], $edit['fields[entity:node:nid][indexed]'], t('nid field is indexed.'));
-        $this->assertEqual($fields['entity:node:title']['indexed'], $edit['fields[entity:node:title][indexed]'], t('title field is indexed.'));
-        $this->assertEqual($fields['entity:node:title']['type'], $edit['fields[entity:node:title][type]'], t('title field type is text.'));
-        $this->assertEqual($fields['entity:node:title']['boost'], $edit['fields[entity:node:title][boost]'], t('title field boost value is 21.'));
-    }
+    $this->assertEqual($fields['entity:node:nid']['indexed'], $edit['fields[entity:node:nid][indexed]'], t('nid field is indexed.'));
+    $this->assertEqual($fields['entity:node:title']['indexed'], $edit['fields[entity:node:title][indexed]'], t('title field is indexed.'));
+    $this->assertEqual($fields['entity:node:title']['type'], $edit['fields[entity:node:title][type]'], t('title field type is text.'));
+    $this->assertEqual($fields['entity:node:title']['boost'], $edit['fields[entity:node:title][boost]'], t('title field boost value is 21.'));
+  }
 
-    public function addAdditionalFieldsToIndex() {
-        // @todo Implement addAdditionalFieldsToIndex() method.
-    }
+  public function addAdditionalFieldsToIndex() {
+    // @todo Implement addAdditionalFieldsToIndex() method.
+  }
 
-    public function renderMenuLinkTest() {
-      $this->drupalGet('admin/config');
-      $this->assertText('Search API', 'Search API menu link is displayed.');
-    }
+  public function addFiltersToIndex() {
+    $settings_path = 'admin/config/search/search_api/index/' . $this->indexId . '/filters';
 
-    public function addFiltersToIndex() {
-      $settings_path = 'admin/config/search/search_api/index/' . $this->index_id . '/filters';
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200);
 
-      $this->drupalGet($settings_path);
-      $this->assertResponse(200);
+    $edit = array(
+      'processors[search_api_highlight_processor][status]' => 1,
+      'processors[search_api_add_aggregation_processor][status]' => 1,
+    );
 
-      $edit = array(
-        'processors[search_api_highlight_processor][status]' => 1,
-        'processors[search_api_add_aggregation_processor][status]' => 1,
-      );
-
-      $this->drupalPostForm($settings_path, $edit, t('Save'));
-      $this->assertText(t('The indexing workflow was successfully edited. All content was scheduled for re-indexing so the new settings can take effect.'));
-      $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->index_id . '/filters') !== FALSE;
-      $this->assertTrue($redirect_path, t('Correct redirect to fields page.'));
-    }
+    $this->drupalPostForm($settings_path, $edit, t('Save'));
+    $this->assertText(t('The indexing workflow was successfully edited. All content was scheduled for re-indexing so the new settings can take effect.'));
+    $redirect_path = strpos($this->getUrl(), 'admin/config/search/search_api/index/' . $this->indexId . '/filters') !== FALSE;
+    $this->assertTrue($redirect_path, t('Correct redirect to fields page.'));
+  }
 }
-
-
