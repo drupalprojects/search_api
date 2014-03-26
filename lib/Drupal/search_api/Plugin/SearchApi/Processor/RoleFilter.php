adb13dc225c620ad3e13c61d0f80056f8f7244b8
Baldwin Louie <baldwinlouie@gmail.com>
2014-03-26 18:08:56 +0100

    ported callback_role_filter.inc to RoleFilter.php. needs testing

---
+++ lib/Drupal/search_api/Plugin/SearchApi/Processor/RoleFilter.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\search_api\Plugin\SearchApi\Processor\RoleFilter
+ */
+
+namespace Drupal\search_api\Plugin\SearchApi\Processor;
+
+use Drupal\search_api\Index\IndexInterface;
+use Drupal\search_api\Processor\FieldsProcessorPluginBase;
+
+ /**
+  * @SearchApiProcessor(
+  *   id = "search_api_role_filter_processor",
+  *   label = @Translation("Role Filter"),
+  *   description = @Translation("Data alteration that filters out users based on their role.")
+  * )
+  */
+class RoleFilter extends FieldsProcessorPluginBase {
+  
+  /**
+   * {@inheritdoc}
+   * This plugin only supports indexes containing users.
+   */
+  public static function supportsIndex(IndexInterface $index) {
+    return $index->getDatasource()->pluginId === 'search_api_content_entity_datasource:user';
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function alterItems(array &$items) {
+    $roles = $this->configuration['roles'];
+    $default = (bool) $this->configuration['default'];
+    foreach ($items as $id => $account) {
+      $role_match = (count(array_diff_key($account->roles, $roles)) !== count($account->roles));
+      if ($role_match === $default) {
+        unset($items[$id]);
+      }
+    }
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, array &$form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    $options = array_map(function ($item) {
+      return check_plain($item->label());
+    }, user_roles());
+
+    $form['default'] = array(
+      '#type' => 'radios',
+      '#title' => t('Which users should be indexed?'),
+      '#default_value' => isset($this->configuration['default']) ? $this->configuration['default'] : 1,
+      '#options' => array(
+        1 => t('All but those from one of the selected roles'),
+        0 => t('Only those from the selected roles'),
+      ),
+    );
+    $form['roles'] = array(
+      '#type' => 'select',
+      '#title' => t('Roles'),
+      '#default_value' => isset($this->configuration['roles']) ? $this->configuration['roles'] : array(),
+      '#options' => $options,
+      '#size' => min(4, count($options)),
+      '#multiple' => TRUE,
+    );
+    return $form;
+  }
+}
