3e664e829ebecf225a0c3892e511f579acc6fabc
Andrei Mateescu <andrei@amateescu.me>
2014-04-14 20:04:19 +0300

    Visual refresh for the overview page to be inline with core.

--- lib/Drupal/search_api/Tests/SearchApiOverviewPageTest.php
+++ lib/Drupal/search_api/Tests/SearchApiOverviewPageTest.php
@@ -54,7 +54,7 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
 
     $this->assertText($server->label(), 'Server present on overview page.');
     $this->assertRaw($server->get('description'), 'Description is present');
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $server->getEntityTypeId() . '-' . $server->id() . '")]//span[@class="search-api-entity-status-enabled"]', NULL, 'Server is in proper table');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $server->getEntityTypeId() . '-' . $server->id() . '") and contains(@class, "search-api-list-enabled")]', NULL, 'Server is in proper table');
 
     // Test the creation of an index.
     $index = $this->getTestIndex();
@@ -63,7 +63,7 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
 
     $this->assertText($index->label(), 'Index present on overview page.');
     $this->assertRaw($index->get('description'), 'Index description is present');
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '")]//span[@class="search-api-entity-status-enabled"]', NULL, 'Index is in proper table');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '") and contains(@class, "search-api-list-enabled")]', NULL, 'Index is in proper table');
   }
 
   /**
@@ -80,13 +80,13 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
     // disabled.
     $server->setStatus(FALSE)->save();
     $this->drupalGet($this->overviewPageUrl);
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $server->getEntityTypeId() . '-' . $server->id() . '")]//span[@class="search-api-entity-status-disabled"]', NULL, 'The server has been disabled.');
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '")]//span[@class="search-api-entity-status-disabled"]', NULL, 'The index has been disabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $server->getEntityTypeId() . '-' . $server->id() . '") and contains(@class, "search-api-list-disabled")]', NULL, 'The server has been disabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '") and contains(@class, "search-api-list-disabled")]', NULL, 'The index has been disabled.');
 
     // Test that an index can't be enabled if its server is disabled.
-    $this->clickLink('enable', 1);
+    $this->clickLink('Enable', 1);
     $this->drupalGet($this->overviewPageUrl);
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '")]//span[@class="search-api-entity-status-disabled"]', NULL, 'The index could not be enabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '") and contains(@class, "search-api-list-disabled")]', NULL, 'The index could not be enabled.');
 
     // Enable the server and try again.
     $server->setStatus(TRUE)->save();
@@ -94,9 +94,9 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
 
     // This time the server is enabled so the first 'enable' link belongs to the
     // index.
-    $this->clickLink('enable');
+    $this->clickLink('Enable');
     $this->drupalGet($this->overviewPageUrl);
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '")]//span[@class="search-api-entity-status-enabled"]', NULL, 'The index has been enabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $index->getEntityTypeId() . '-' . $index->id() . '") and contains(@class, "search-api-list-enabled")]', NULL, 'The index has been enabled.');
 
     // Create a new index without a server assigned and test that it can't be
     // enabled. The overview UI is not very consistent at the moment, so test
@@ -116,31 +116,31 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
    */
   protected function assertEntityStatusChange($entity) {
     $this->drupalGet($this->overviewPageUrl);
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '")]//span[@class="search-api-entity-status-enabled"]', NULL, 'The newly created entity is enabled by default.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '") and contains(@class, "search-api-list-enabled")]', NULL, 'The newly created entity is enabled by default.');
 
     // The first 'disable' link on the page belongs to our newly created server
     // and the second 'disable' link belongs to our newly created index.
     if ($entity instanceof ServerInterface) {
-      $this->clickLink('disable');
+      $this->clickLink('Disable');
     }
     else {
-      $this->clickLink('disable', 1);
+      $this->clickLink('Disable', 1);
     }
 
     // Submit the confirmation form and test that entity has been disabled.
     $this->drupalPostForm(NULL, array(), 'Disable');
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '")]//span[@class="search-api-entity-status-disabled"]', NULL, 'The entity has been disabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '") and contains(@class, "search-api-list-disabled")]', NULL, 'The entity has been disabled.');
 
     // Now enable the entity.
-    $this->clickLink('enable');
+    $this->clickLink('Enable');
 
     // And test that the enable operation succeeded.
     $this->drupalGet($this->overviewPageUrl);
-    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '")]//span[@class="search-api-entity-status-enabled"]', NULL, 'The entity has benn enabled.');
+    $this->assertFieldByXPath('//tr[contains(@class,"' . $entity->getEntityTypeId() . '-' . $entity->id() . '") and contains(@class, "search-api-list-enabled")]', NULL, 'The entity has benn enabled.');
   }
 
   /**
-   * Tests index and server operations in the overview page.
+   * Tests server operations in the overview page.
    */
   public function testOperations() {
     /** @var $server \Drupal\search_api\Server\ServerInterface */
@@ -148,18 +148,17 @@ class SearchApiOverviewPageTest extends SearchApiWebTestBase {
 
     $this->drupalGet($this->overviewPageUrl);
     $basic_url = $this->urlGenerator->generateFromRoute('search_api.server_view', array('search_api_server' => $server->id()));
-    $this->assertRaw('<a href="' . $basic_url . '">canonical</a>', 'Canonical operation presents');
-    $this->assertRaw('<a href="' . $basic_url . '/edit">edit-form</a>', 'Edit operation presents');
-    $this->assertRaw('<a href="' . $basic_url . '/disable">disable</a>', 'Disable operation presents');
-    $this->assertRaw('<a href="' . $basic_url . '/delete">delete-form</a>', 'Delete operation presents');
-    $this->assertNoRaw('<a href="' . $basic_url . '/enable">enabled-form</a>', 'Enable operation is not present');
+    $this->assertRaw('<a href="' . $basic_url . '/edit">Edit</a>', 'Edit operation presents');
+    $this->assertRaw('<a href="' . $basic_url . '/disable">Disable</a>', 'Disable operation presents');
+    $this->assertRaw('<a href="' . $basic_url . '/delete">Delete</a>', 'Delete operation presents');
+    $this->assertNoRaw('<a href="' . $basic_url . '/enable">Enable</a>', 'Enable operation is not present');
 
-    $server->disable()->save();
+    $server->setStatus(FALSE)->save();
     $this->drupalGet($this->overviewPageUrl);
 
     // As CsrfTokenGenerator uses current session Id we can not generate valid token
     $this->assertRaw('<a href="' . $basic_url .'/enable?token=', 'Enable operation present');
-    $this->assertNoRaw('<a href="' . $basic_url .'/disable">disable-form</a>', 'Disable operation  is not present');
+    $this->assertNoRaw('<a href="' . $basic_url .'/disable">Disable</a>', 'Disable operation  is not present');
   }
 
 }
