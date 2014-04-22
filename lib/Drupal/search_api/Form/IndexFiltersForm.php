63e0ebda1d46133d6c340240d2899aca2aee6d17
Andrei Mateescu <andrei@amateescu.me>
2014-04-22 17:39:27 +0300

    Started some work on the Filters page.

--- lib/Drupal/search_api/Form/IndexFiltersForm.php
+++ lib/Drupal/search_api/Form/IndexFiltersForm.php
@@ -34,11 +34,18 @@ class IndexFiltersForm extends EntityFormController {
   protected $entityManager;
 
   /**
-   * The datasource manager.
+   * The processor plugin manager.
    *
    * @var \Drupal\search_api\Processor\ProcessorPluginManager
    */
   protected $processorPluginManager;
+
+  /**
+   * The ID of the datasource plugin.
+   *
+   * @var string
+   */
+  protected $datasourceId;
 
   /**
    * Constructs a ContentEntityFormController object.
@@ -73,7 +80,14 @@ class IndexFiltersForm extends EntityFormController {
   /**
    * {@inheritdoc}
    */
-  public function form(array $form, array &$form_state) {
+  public function form(array $form, array &$form_state, $datasource_id = NULL) {
+    // Set a default datasource plugin ID if none was passed in.
+    if (!$datasource_id) {
+      $datasource_ids = $this->entity->getDatasourceIds();
+      $datasource_id = reset($datasource_ids);
+    }
+    $this->datasourceId = $datasource_id;
+
     // Fetch our active Processors for this index
     $processors_by_weight = $this->entity->getProcessors(TRUE, 'weight');
     // Fetch all processors
