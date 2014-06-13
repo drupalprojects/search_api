7aa2949635f659d9b0d58c2a9a5100a4e0376c54
Thomas Seidl <remus@gmx.net>
2014-06-13 20:46:54 +0200

    Refactored and expanded SearchApiServerTaskUnitTest.

--- src/Tests/SearchApiServerTaskUnitTest.php
+++ src/Tests/SearchApiServerTaskUnitTest.php
@@ -95,48 +95,85 @@ class SearchApiServerTaskUnitTest extends EntityUnitTestBase {
   /**
    * Tests task system integration for the server's addIndex() method.
    */
-  public function testTaskCreation() {
+  public function testAddIndex() {
     // Since we want to add the index, we should first remove it (even though it
     // shouldn't matter – just for logic consistency).
     $this->index->setServer(NULL);
     $this->index->save();
 
-    // Set exception for addIndex() calls and reset the list of successful
+    $this->checkTaskSystem('addIndex');
+  }
+
+  /**
+   * Tests task system integration for the server's updateIndex() method.
+   */
+  public function testUpdateIndex() {
+    // Since we want to add the index, we should first remove it (even though it
+    // shouldn't matter – just for logic consistency).
+    $this->index->setServer(NULL);
+    $this->index->save();
+
+    $this->checkTaskSystem('updateIndex', 'addIndex', 'updateIndex');
+  }
+
+  /**
+   * Checks server task system integration for a single method.
+   *
+   * Used as a helper to avoid code duplication when checking all the methods.
+   *
+   * @param string $type
+   *   One of the methods that has server task system integration – "addIndex",
+   *   "updateIndex", "removeIndex", "deleteItems" or "deleteAllIndexItems".
+   * @param string $second_type
+   *   (optional) A second type to use for the second call.
+   * @param string|null $third_type
+   *   (optional) A third type to use for final triggering of task execution. If
+   *   not given, use a cron run instead.
+   */
+  public function checkTaskSystem($type, $second_type = 'updateIndex', $third_type = NULL) {
+    // Set exception for the tested method and reset the list of successful
     // backend method calls.
-    $this->state->set('search_api_test_backend.exception.addIndex', TRUE);
+    $this->state->set("search_api_test_backend.exception.$type", TRUE);
     $this->getCalledServerMethods();
 
     // Try to add an index.
-    $this->server->addIndex($this->index);
-    $this->assertEqual($this->getCalledServerMethods(), array(), 'addIndex correctly threw an exception.');
+    $this->server->$type($this->index);
+    $this->assertEqual($this->getCalledServerMethods(), array(), "$type correctly threw an exception.");
     $tasks = $this->getServerTasks();
     if (count($tasks) == 1) {
-      $task_created = $tasks[0]->type === 'addIndex';
+      $task_created = $tasks[0]->type === $type;
     }
-    $this->assertTrue(!empty($task_created), 'The addIndex task was successfully added.');
+    $this->assertTrue(!empty($task_created), "The $type task was successfully added.");
     if ($tasks) {
-      $this->assertEqual($tasks[0]->index_id, $this->index->id(), 'The right index ID was used for the addIndex task.');
+      $this->assertEqual($tasks[0]->index_id, $this->index->id(), "The right index ID was used for the $type task.");
     }
 
     // Check whether other task-system-integrated methods now fail, too.
-    $this->server->updateIndex($this->index);
-    $this->assertEqual($this->getCalledServerMethods(), array(), 'updateIndex was not executed.');
+    $this->server->$second_type($this->index);
+    $this->assertEqual($this->getCalledServerMethods(), array(), "$second_type was not executed.");
     $tasks = $this->getServerTasks();
     if (count($tasks) == 2) {
-      $this->pass('Second task (updateIndex) was added.');
-      $this->assertEqual($tasks[0]->type, 'addIndex', 'First task stayed the same.');
-      $this->assertEqual($tasks[1]->type, 'updateIndex', 'New task was queued as last.');
+      $this->pass("Second task ($second_type) was added.");
+      $this->assertEqual($tasks[0]->type, $type, 'First task stayed the same.');
+      $this->assertEqual($tasks[1]->type, $second_type, 'New task was queued as last.');
     }
     else {
-      $this->fail('Second task (updateIndex) was not added.');
+      $this->fail("Second task ($second_type) was not added.");
     }
 
-    // Let addIndex() succeed again and check if a cron run properly executes
-    // the tasks.
-    $this->state->set('search_api_test_backend.exception.addIndex', FALSE);
-    search_api_cron();
-    $this->assertEqual($this->getServerTasks(), array(), 'Server tasks were correctly executed during cron run.');
-    $this->assertEqual($this->getCalledServerMethods(), array('addIndex', 'updateIndex'), 'Right methods were called during task execution.');
+    // Let $type() succeed again, then trigger the task execution either with
+    // a third method call or a cron run.
+    $this->state->set("search_api_test_backend.exception.$type", FALSE);
+    $expected_methods = array($type, $second_type);
+    if ($third_type) {
+      $expected_methods[] = $third_type;
+      $this->server->$third_type($this->index);
+    }
+    else {
+      search_api_cron();
+    }
+    $this->assertEqual($this->getServerTasks(), array(), 'Server tasks were correctly executed.');
+    $this->assertEqual($this->getCalledServerMethods(), $expected_methods, 'Right methods were called during task execution.');
   }
 
   /**
