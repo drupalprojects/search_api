8e6f7682b88dcd2cd2cc10f80fec6da9b4d7aa83
nickveenhof <nick.veenhof@gmail.com>
2014-06-15 14:18:00 +0200

    Adding tests for the ignorecharacter so that multiple character sets get added

--- tests/src/Plugin/Processor/IgnoreCharacterTest.php
+++ tests/src/Plugin/Processor/IgnoreCharacterTest.php
@@ -54,10 +54,10 @@ class IgnoreCharacterTest extends UnitTestCase {
    *
    * @dataProvider textCharacterSetsIgnoreCharacterDataProvider
    */
-  public function testCharacterSetsIgnoreCharacter($passedString, $expectedValue, $character_class) {
+  public function testCharacterSetsIgnoreCharacter($passedString, $expectedValue, $character_classes) {
     $tokenizerMock = $this->getMock('Drupal\search_api\Plugin\SearchApi\Processor\IgnoreCharacter',
       array('processFieldValue'),
-      array(array('strip' => array('ignorable' => "['¿¡!?,.:;]", 'character_sets' => array($character_class => $character_class))), 'string', array()));
+      array(array('strip' => array('ignorable' => "['¿¡!?,.:;]", 'character_sets' => $character_classes)), 'string', array()));
 
     $processFieldValueMethod = $this->getAccessibleMethod('processFieldValue');
     // Decode entities to UTF-8
@@ -71,56 +71,59 @@ class IgnoreCharacterTest extends UnitTestCase {
    */
   public function textCharacterSetsIgnoreCharacterDataProvider() {
     return array(
-      array('word_s', 'words', 'Pc'),
-      array('word⁔s', 'words', 'Pc'),
+      array('word_s', 'words', array('Pc' => 'Pc')),
+      array('word⁔s', 'words', array('Pc' => 'Pc')),
 
-      array('word〜s', 'words', 'Pd'),
-      array('w–ord⸗s', 'words', 'Pd'),
+      array('word〜s', 'words', array('Pd' => 'Pd')),
+      array('w–ord⸗s', 'words', array('Pd' => 'Pd')),
 
-      array('word⌉s', 'words', 'Pe'),
-      array('word⦊s〕', 'words', 'Pe'),
+      array('word⌉s', 'words', array('Pe' => 'Pe')),
+      array('word⦊s〕', 'words', array('Pe' => 'Pe')),
 
-      array('word»s', 'words', 'Pf'),
-      array('word⸍s', 'words', 'Pf'),
+      array('word»s', 'words', array('Pf' => 'Pf')),
+      array('word⸍s', 'words', array('Pf' => 'Pf')),
 
-      array('word⸂s', 'words', 'Pi'),
-      array('w«ord⸉s', 'words', 'Pi'),
+      array('word⸂s', 'words', array('Pi' => 'Pi')),
+      array('w«ord⸉s', 'words', array('Pi' => 'Pi')),
 
-      array('words%', 'words', 'Po'),
-      array('wo*rd/s', 'words', 'Po'),
+      array('words%', 'words', array('Po' => 'Po')),
+      array('wo*rd/s', 'words', array('Po' => 'Po')),
 
-      array('word༺s', 'words', 'Ps'),
-      array('w❮ord⌈s', 'words', 'Ps'),
+      array('word༺s', 'words', array('Ps' => 'Ps')),
+      array('w❮ord⌈s', 'words', array('Ps' => 'Ps')),
 
-      array('word៛s', 'words', 'Sc'),
-      array('wo₥rd₦s', 'words', 'Sc'),
+      array('word៛s', 'words', array('Sc' => 'Sc')),
+      array('wo₥rd₦s', 'words', array('Sc' => 'Sc')),
 
-      array('w˓ords', 'words', 'Sk'),
-      array('wo˘rd˳s', 'words', 'Sk'),
+      array('w˓ords', 'words', array('Sk' => 'Sk')),
+      array('wo˘rd˳s', 'words', array('Sk' => 'Sk')),
 
-      array('word×s', 'words', 'Sm'),
-      array('wo±rd؈s', 'words', 'Sm'),
+      array('word×s', 'words', array('Sm' => 'Sm')),
+      array('wo±rd؈s', 'words', array('Sm' => 'Sm')),
 
-      array('wo᧧rds', 'words', 'So'),
-      array('w᧶ord᧲s', 'words', 'So'),
+      array('wo᧧rds', 'words', array('So' => 'So')),
+      array('w᧶ord᧲s', 'words', array('So' => 'So')),
 
       //array('worœds', 'words', 'Cc'),
       //array('woƒrds', 'words', 'Cc'),
 
-      array('word۝s', 'words', 'Cf'),
-      array('wo᠎rd؁s', 'words', 'Cf'),
+      array('word۝s', 'words', array('Cf' => 'Cf')),
+      array('wo᠎rd؁s', 'words', array('Cf' => 'Cf')),
 
-      array('words', 'words', 'Co'),
-      array('wo󿿽rds', 'words', 'Co'),
+      array('words', 'words', array('Co' => 'Co')),
+      array('wo󿿽rds', 'words', array('Co' => 'Co')),
 
-      array('wordॊs', 'words', 'Mc'),
-      array('worौdংs', 'words', 'Mc'),
+      array('wordॊs', 'words', array('Mc' => 'Mc')),
+      array('worौdংs', 'words', array('Mc' => 'Mc')),
 
-      array('wo⃞rds', 'words', 'Me'),
-      array('wor⃤⃟ds', 'words', 'Me'),
+      array('wo⃞rds', 'words', array('Me' => 'Me')),
+      array('wor⃤⃟ds', 'words', array('Me' => 'Me')),
 
-      array('woྰrds', 'words', 'Mn'),
-      array('worྵdྶs', 'words', 'Mn'),
+      array('woྰrds', 'words', array('Mn' => 'Mn')),
+      array('worྵdྶs', 'words', array('Mn' => 'Mn')),
+
+      array('woྰrds', 'words', array('Mn' => 'Mn', 'Pd' => 'Pd', 'Pe' => 'Pe')),
+      array('worྵdྶs', 'words', array('Mn' => 'Mn', 'Pd' => 'Pd', 'Pe' => 'Pe')),
     );
   }
 
