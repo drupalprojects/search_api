db736baf40fbfe6055b50ae6c29b7773f5b3a6e0
Joris Vercammen <joris.vercammen@intracto.com>
2014-11-07 10:52:49 +0100

    Issue #2304963: Add integration test for url field, content access, language

---
+++ src/Tests/Processor/ProcessorIntegrationTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\search_api\Tests\Processor;
+
+use Drupal\search_api\Tests\IntegrationTest;
+
+class ProcessorIntegrationTest extends IntegrationTest {
+
+  /**
+   * Creates a search api server and index, and tracks content
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->drupalLogin($this->adminUser);
+
+    $this->serverId = $this->createServer();
+    $this->createIndex();
+    $this->trackContent();
+  }
+
+  public function testNodeStatusIntegration() {
+    $this->addFilter('node_status');
+  }
+
+  public function testIgnoreCaseIntegration() {
+    $this->addFilter('ignorecase');
+
+    $settings_path = 'admin/config/search/search-api/index/' . $this->indexId . '/filters';
+
+    $edit = array(
+      'processors[ignorecase][settings][fields][search_api_language]' => FALSE,
+    );
+    $this->drupalPostForm($settings_path, $edit, $this->t('Save'));
+
+    /** @var \Drupal\search_api\Index\IndexInterface $index */
+    $index = entity_load('search_api_index', $this->indexId, TRUE);
+    $processors = $index->getProcessors();
+    if (isset($processors['ignorecase'])) {
+      $configuration = $processors['ignorecase']->getConfiguration();
+      $this->assertTrue(empty($configuration['fields']['search_api_language']), 'Language field disabled for ignore case filter.');
+    }
+    else {
+      $this->fail('"Ignore case" processor not enabled.');
+    }
+  }
+
+  public function testUrlFieldIntegration() {
+    $this->addFilter('add_url');
+  }
+
+  public function testContentAccessIntegration() {
+    $this->addFilter('content_access');
+  }
+
+  public function testLanguageIntegration() {
+    $this->addFilter('language');
+  }
+
+}
