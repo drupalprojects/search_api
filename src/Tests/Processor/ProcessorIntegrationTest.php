a7bf4439877a7b59bb0f83da321811f551ce779f
Joris Vercammen <joris.vercammen@intracto.com>
2014-11-09 13:36:36 +0100

    Issue #2304963: Clean up integration test, remove unneeded assertions

--- src/Tests/Processor/ProcessorIntegrationTest.php
+++ src/Tests/Processor/ProcessorIntegrationTest.php
@@ -2,20 +2,40 @@
 
 namespace Drupal\search_api\Tests\Processor;
 
-use Drupal\search_api\Tests\IntegrationTestBase;
+use Drupal\Component\Utility\Unicode;
+use Drupal\search_api\Tests\SearchApiWebTestBase;
 
 /**
  * Tests the processors functionality of the Search API backend.
  *
  * @group search_api
  */
-class ProcessorIntegrationTest extends IntegrationTestBase {
+class ProcessorIntegrationTest extends SearchApiWebTestBase {
+
+  /**
+   * A search server ID.
+   *
+   * @var string
+   */
+  protected $serverId;
+
+  /**
+   * A search index ID.
+   *
+   * @var string
+   */
+  protected $indexId;
 
   /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
+    $this->drupalLogin($this->adminUser);
+
+    $this->createServer();
+    $this->createIndex();
+    $this->trackContent();
   }
 
   public function testNodeStatusIntegration() {
@@ -142,6 +162,141 @@ class ProcessorIntegrationTest extends IntegrationTestBase {
       $this->fail($processorName . ' settings not applied.');
     }
 
+  }
+
+  /**
+   * Test that a filter can be added.
+   */
+  private function addFilter($filterName) {
+    // Go to the index filter path
+    $settings_path = 'admin/config/search/search-api/index/' . $this->indexId . '/filters';
+
+    $edit = array(
+      'processors['.$filterName.'][status]' => 1,
+    );
+    $this->drupalPostForm($settings_path, $edit, $this->t('Save'));
+    /** @var \Drupal\search_api\Index\IndexInterface $index */
+    $index = entity_load('search_api_index', $this->indexId, TRUE);
+    $processors = $index->getProcessors();
+    $this->assertTrue(isset($processors[$filterName]), ucfirst($filterName) . ' processor enabled');
+  }
+
+  /**
+   * Creates a server, so we can test the processors trough the UI
+   */
+  protected function createServer() {
+    $this->serverId = Unicode::strtolower($this->randomMachineName());
+    $settings_path = $this->urlGenerator->generateFromRoute('entity.search_api_server.add_form', array(), array('absolute' => TRUE));
+
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200, 'Server add page exists');
+
+    $edit = array(
+      'name' => '',
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'backend' => 'search_api_test_backend',
+    );
+
+    $this->drupalPostForm($settings_path, $edit, $this->t('Save'));
+    $this->assertText($this->t('!name field is required.', array('!name' => $this->t('Server name'))));
+
+    $edit = array(
+      'name' => 'Search API test server',
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'backend' => 'search_api_test_backend',
+    );
+    $this->drupalPostForm($settings_path, $edit, $this->t('Save'));
+    $this->assertText($this->t('!name field is required.', array('!name' => $this->t('Machine-readable name'))));
+
+    $edit = array(
+      'name' => 'Search API test server',
+      'machine_name' => $this->serverId,
+      'status' => 1,
+      'description' => 'A server used for testing.',
+      'backend' => 'search_api_test_backend',
+    );
+
+    $this->drupalPostForm(NULL, $edit, $this->t('Save'));
+
+    $this->assertText($this->t('The server was successfully saved.'));
+  }
+
+  /**
+   * Creates an index, so we can test the processors trough the UI
+   */
+  protected function createIndex() {
+    $settings_path = $this->urlGenerator->generateFromRoute('entity.search_api_index.add_form', array(), array('absolute' => TRUE));
+
+    $this->drupalGet($settings_path);
+    $this->assertResponse(200);
+    $edit = array(
+      'status' => 1,
+      'description' => 'An index used for testing.',
+    );
+
+    $this->drupalPostForm(NULL, $edit, $this->t('Save'));
+    $this->assertText($this->t('!name field is required.', array('!name' => $this->t('Index name'))));
+    $this->assertText($this->t('!name field is required.', array('!name' => $this->t('Machine-readable name'))));
+    $this->assertText($this->t('!name field is required.', array('!name' => $this->t('Data types'))));
+
+    $this->indexId = 'test_index';
+
+    $edit = array(
+      'name' => 'Search API test index',
+      'machine_name' => $this->indexId,
+      'status' => 1,
+      'description' => 'An index used for testing.',
+      'server' => $this->serverId,
+      'datasources[]' => array('entity:node'),
+    );
+
+    $this->drupalPostForm(NULL, $edit, $this->t('Save'));
+
+    $this->assertText($this->t('The index was successfully saved.'));
+    $this->assertUrl('admin/config/search/search-api/index/' . $this->indexId, array(), $this->t('Correct redirect to index page.'));
+  }
+
+  /**
+   * Adds content to index.
+   */
+  private function trackContent() {
+    // Initially there should be no tracked items, because there are no nodes
+    $tracked_items = $this->countTrackedItems();
+
+    $this->assertEqual($tracked_items, 0, $this->t('No items are tracked yet'));
+
+    // Add two articles and a page
+    $this->drupalCreateNode(array('type' => 'article'));
+    $this->drupalCreateNode(array('type' => 'article'));
+    $this->drupalCreateNode(array('type' => 'page'));
+
+    // Those 3 new nodes should be added to the index immediately
+    $tracked_items = $this->countTrackedItems();
+    $this->assertEqual($tracked_items, 3, $this->t('Three items are tracked'));
+  }
+
+  /**
+   * Counts the number of tracked items from an index.
+   *
+   * @return int
+   */
+  private function countTrackedItems() {
+    /** @var $index \Drupal\search_api\Index\IndexInterface */
+    $index = entity_load('search_api_index', $this->indexId);
+    return $index->getTracker()->getTotalItemsCount();
+  }
+
+  /**
+   * Counts the number of remaining items from an index.
+   *
+   * @return int
+   */
+  private function countRemainingItems() {
+    /** @var $index \Drupal\search_api\Index\IndexInterface */
+    $index = entity_load('search_api_index', $this->indexId);
+    return $index->getTracker()->getRemainingItemsCount();
   }
 
 }
