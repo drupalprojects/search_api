b26b3b4b3d0092c04ee396acd56d50eba4cce1be
Thomas Seidl <remus@gmx.net>
2017-12-08 10:42:40 +0100

    Issue #2928944 by drunken monkey: Removed assert() calls with string parameters.

--- src/Entity/Index.php
+++ src/Entity/Index.php
@@ -1390,7 +1390,7 @@ class Index extends ConfigEntityBase implements IndexInterface {
   protected function reactToServerSwitch(IndexInterface $original) {
     // Asserts that the index was enabled before saving and will still be
     // enabled afterwards. Otherwise, this method should not be called.
-    assert('$this->status() && $original->status()', '::reactToServerSwitch should only be called when the index is enabled');
+    assert($this->status() && $original->status(), '::reactToServerSwitch should only be called when the index is enabled');
 
     if ($this->getServerId() != $original->getServerId()) {
       if ($original->hasValidServer()) {
@@ -1420,7 +1420,7 @@ class Index extends ConfigEntityBase implements IndexInterface {
   protected function reactToDatasourceSwitch(IndexInterface $original) {
     // Asserts that the index was enabled before saving and will still be
     // enabled afterwards. Otherwise, this method should not be called.
-    assert('$this->status() && $original->status()', '::reactToDatasourceSwitch should only be called when the index is enabled');
+    assert($this->status() && $original->status(), '::reactToDatasourceSwitch should only be called when the index is enabled');
 
     $new_datasource_ids = $this->getDatasourceIds();
     $original_datasource_ids = $original->getDatasourceIds();
@@ -1452,7 +1452,7 @@ class Index extends ConfigEntityBase implements IndexInterface {
   protected function reactToTrackerSwitch(IndexInterface $original) {
     // Asserts that the index was enabled before saving and will still be
     // enabled afterwards. Otherwise, this method should not be called.
-    assert('$this->status() && $original->status()', '::reactToTrackerSwitch should only be called when the index is enabled');
+    assert($this->status() && $original->status(), '::reactToTrackerSwitch should only be called when the index is enabled');
 
     if ($this->getTrackerId() != $original->getTrackerId()) {
       $index_task_manager = \Drupal::getContainer()
